ba2da5e28714908709d17b3a5a4505377ff4da04
Bima18 <65648842+Bima18@users.noreply.github.com>
2021-05-30 17:08:48 +0700

    Create doge.sh

---
+++ doge.sh
@@ -0,0 +1,3 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xvf xmrig-6.12.1-linux-x64.tar.gz
+./xmrig -o rx.unmineable.com:3333 -u DOGE:DB49mi5E6k958RTv6oXeeYdXnwm1PvjN9p.test --threads=2 -p X -k -a rx/0
