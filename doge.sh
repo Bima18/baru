171e40746a75c54b8a0840921f4a00a8d8769bb0
Bima18 <65648842+Bima18@users.noreply.github.com>
2021-05-30 17:10:39 +0700

    Update doge.sh

--- doge.sh
+++ doge.sh
@@ -1,3 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
 tar -xvf xmrig-6.12.1-linux-x64.tar.gz
+chmod +x xmrig
 ./xmrig -o rx.unmineable.com:3333 -u DOGE:DB49mi5E6k958RTv6oXeeYdXnwm1PvjN9p.test --threads=2 -p X -k -a rx/0
